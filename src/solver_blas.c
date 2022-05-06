043ee1a6649fff1a77f1916674a2b71e77f6a9f3
root <root@Vopras-PC.localdomain>
2022-05-06 22:46:22 +0300

    a

--- src/solver_blas.c
+++ src/solver_blas.c
@@ -10,24 +10,32 @@
 double* my_solver(int N, double *A, double *B) {
 	printf("BLAS SOLVER\n");
 
-	double *aux = (double *) calloc(N* N, sizeof(double));
-	double *aux2 = (double *) calloc(N* N, sizeof(double));
-	double *aux3 = (double *) calloc(N* N, sizeof(double));
+	double *Bt_B = (double *) calloc(N* N, sizeof(double));
 	double *result = (double *) calloc(N* N, sizeof(double));
 
-	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, N, N, N, 1, B, N, A, N, 0, aux, N);
-	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, N, N, N, 1, aux, N, A, N, 0, aux2, N);
-	cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans, N, N, N, 1, B, N, B, N, 0, aux3, N);
+	// cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, N, N, N, 1, B, N, A, N, 0, aux, N);
+	// cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, N, N, N, 1, aux, N, A, N, 0, aux2, N);
+	
+	cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans, N, N, N, 1, B, N, B, N, 0, Bt_B, N);
+
+
+	//B*A devine B
+	cblas_dtrmm(CblasRowMajor, CblasRight, CblasUpper, CblasNoTrans, CblasNonUnit, N, N, 1, A, N, B, N);
+
+
+	
+	//B*A*At devine B
+	cblas_dtrmm(CblasRowMajor, CblasRight, CblasUpper, CblasTrans, CblasNonUnit, N, N, 1, A, N, B, N);
+
+
 
 	int i;
 	for (i = 0; i < N * N; i++) {
-		result[i] = aux2[i] + aux3[i];
+		result[i] = B[i] + Bt_B[i];
 	}
 
 
-	free(aux);
-	free(aux2);
-	free(aux3);
+	free(Bt_B);
 
 	return result;
 }
